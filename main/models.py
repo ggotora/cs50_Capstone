1c807c7845b71b3ccb11992581b73cea08bd60de
Gilbert Gotora <gilbertgotora2@gmail.com>
2024-11-18 02:03:04 +0200

    install Pillow, Profile model and profile_save signal

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.urls import reverse
 
-# Create your models here.
+class User(AbstractUser):
+    pass 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    organisation = models.CharField(max_length=50)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
+    image = models.ImageField(default='profile_pics/default.png', upload_to='profile_pics')
+    created = models.DateTimeField(auto_now_add=True)
+ 
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def get_absolute_url(self):
+        return reverse("loads:profile_detail", kwargs={"pk": self.pk})
+    
+
+def create_user_profile(sender, created,instance,  *args, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
